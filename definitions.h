254b591fbea59690805aa8b24d2c0462bc11e3ba
Khral <khral_21@yahoo.fr>
2014-12-01 10:21:12 +0100

    Ajout définitions

---
+++ definitions.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   definitions.h
+ * Author: Etienne
+ *
+ * Created on 1 décembre 2014, 09:58
+ */
+
+#ifndef DEFINITIONS_H
+#define	DEFINITIONS_H
+
+const int TAILLE=5;
+
+enum JOUEUR {BLANC, NOIR};
+
+class Coup{
+private:
+    int joueur;
+    int x;
+    int y;
+};
+
+#endif	/* DEFINITIONS_H */
+
--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@
 
 #include <cstdlib>
 
+#include "definitions.h"
+
 using namespace std;
 
-/*
- * 
- */
+
 int main(int argc, char** argv) {
 
+    //Initialisation du plateau
+    int plateau[TAILLE][TAILLE];
+    
     return 0;
 }
 
