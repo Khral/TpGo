e0335af86595b3287fe11ffd0f8defe16b28b80c
Valentin L <valentin.lievin@gmail.com>
2014-12-01 10:41:33 +0100

    première version de la fonction affichage

--- affichage.cpp
+++ affichage.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   affichage.cpp
  * Author: Etienne et Samuel
  *
@@ -6,5 +6,36 @@
  */
 
 using namespace std;
+
+void affichagePlateau(int plateau[TAILLE][TAILLE]){
+    int i,j;
+
+    for(int i=0; i<TAILLE; i++)
+    {
+        for(int j=0; j<TAILLE; j++)
+        {
+            if(plateau[i][j] == NULL)
+            {
+                cout << '.';
+            }
+           if(plateau[i][j]== JOUEURNOIR){
+                 cout <<  "N";
+           }
+
+           if(plateau[i][j]== JOUEURBLANC)
+            {
+                cout << "B";
+            }
+            else{
+                cout <<"X"; // permet de gérer le cas où
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
+
+}
 
 
