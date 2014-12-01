260247a9a0012c5b7d9360927f68bccc58622ce5
Valentin L <valentin.lievin@gmail.com>
2014-12-01 10:20:17 +0100

    effacer les commentaires superflus en tête du main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-/* 
- * File:   main.cpp
- * Author: Etienne et Samuel
- *
- * Created on 1 décembre 2014, 09:37
- */
 
 #include <cstdlib>
 
 using namespace std;
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
 
