b0f0690469ec6059637044f0c037e91f0b8543e9
Khral <khral_21@yahoo.fr>
2014-12-01 09:44:29 +0100

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 1 décembre 2014, 09:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
