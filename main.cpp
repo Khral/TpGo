b6de1a390aee91c1e90c2d05be745f1213933d4d
Valentin L <valentin.lievin@gmail.com>
2014-12-03 09:33:01 +0100

    suppression de lignes et test du main

--- main.cpp
+++ main.cpp
@@ -20,11 +20,23 @@ int main(int argc, char** argv) {
 
     //Initialisation de la partie
     Partie partie;
-
-    affichagePlateau(partie.getPlateau());
-    partie.coupUtilisateur(NOIR);
     affichagePlateau(partie.getPlateau());
 
+    // Boucle de jeu
+    int k=0;
+    while (k<10){ // remplacer par une condition de fin de partie
+        if (k%2==0){
+            cout << "Au joueur noir de jouer: " ;
+            partie.coupUtilisateur(NOIR);
+            affichagePlateau(partie.getPlateau());
+        }
+        else {
+            cout << "Au joueur blanc de jouer: " ;
+            partie.coupUtilisateur(BLANC);
+            affichagePlateau(partie.getPlateau());
+        }
+        k++;
+    }
     return 0;
 }
 
--- partie.cpp
+++ partie.cpp
@@ -63,11 +63,6 @@ void Partie::coupUtilisateur (Joueur jouerCourrant){
     listeCoups.push_back(Coupcourrant);
 }
 
-void Partie::actualiserPlateau() { // EN COURS
-    /*<std::vector<std::vector<Joueur> >  newPlateau;
-    listePlateaux.push_back(newPlateau)
-    */
-}
 
 Partie::~Partie() {
 }
